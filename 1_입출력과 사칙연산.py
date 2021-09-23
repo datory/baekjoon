b2338647afefa9ddd88ecd82b7c957640172ff64
datory <sehwan622@gmail.com>
2021-09-23 22:12:01 +0900

    multiply

--- 1_입출력과 사칙연산.py
+++ 1_입출력과 사칙연산.py
@@ -1,8 +1,11 @@
 print("Hello World!")
 print("강한친구 대한육군\n강한친구 대한육군")
 
-a, b = map(int, input().split())
-print(a+b)
+A, B = map(int, input().split())
+print(A+B)
 
 A, B = map(int, input().split())
 print(A-B)
+
+A, B = map(int, input().split())
+print(A+B)
