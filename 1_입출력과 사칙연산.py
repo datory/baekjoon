9a31ba50d6170306ad41ea5842a2f1a37ccee6fc
datory <sehwan622@gmail.com>
2021-09-25 00:27:12 +0900

    arithmetic operation

--- 1_입출력과 사칙연산.py
+++ 1_입출력과 사칙연산.py
@@ -8,7 +8,15 @@ A, B = map(int, input().split())
 print(A-B)
 
 A, B = map(int, input().split())
-print(A+B)
+print(A*B)
 
 A, B = map(int, input().split())
 print(A/B)
+
+A, B = map(int, input().split())
+if A >= 1 and A <= 10000 and B >=1 and B <= 10000:
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(A//B)
+    print(A%B)
