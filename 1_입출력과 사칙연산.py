b7b65edbafcd88729a85f94b1ab073417b5ab732
datory <sehwan622@gmail.com>
2021-09-26 23:36:13 +0900

    remainder

--- 1_입출력과 사칙연산.py
+++ 1_입출력과 사칙연산.py
@@ -1,22 +1,29 @@
-print("Hello World!")
-print("강한친구 대한육군\n강한친구 대한육군")
+# print("Hello World!")
+# print("강한친구 대한육군\n강한친구 대한육군")
 
-A, B = map(int, input().split())
-print(A+B)
+# A, B = map(int, input().split())
+# print(A+B)
 
-A, B = map(int, input().split())
-print(A-B)
+# A, B = map(int, input().split())
+# print(A-B)
 
-A, B = map(int, input().split())
-print(A*B)
+# A, B = map(int, input().split())
+# print(A*B)
 
-A, B = map(int, input().split())
-print(A/B)
+# A, B = map(int, input().split())
+# print(A/B)
 
-A, B = map(int, input().split())
-if A >= 1 and A <= 10000 and B >=1 and B <= 10000:
-    print(A+B)
-    print(A-B)
-    print(A*B)
-    print(A//B)
-    print(A%B)
+# A, B = map(int, input().split())
+# if A >= 1 and A <= 10000 and B >=1 and B <= 10000:
+#     print(A+B)
+#     print(A-B)
+#     print(A*B)
+#     print(A//B)
+#     print(A%B)
+
+A, B, C = map(int, input().split())
+if A >= 2 and A <= 10000 and B >= 2 and B <= 10000 and C >= 2 and C <= 10000:
+    print((A+B)%C)
+    print(((A%C)+(B%C))%C)
+    print((A*B)%C)
+    print(((A%C)*(B%C))%C)
