9f595077ffdf5ad3c5e9b5215ed16359dbca82b9
datory <sehwan622@gmail.com>
2021-09-30 22:54:45 +0900

    print cat

--- 1_입출력과 사칙연산.py
+++ 1_입출력과 사칙연산.py
@@ -1,18 +1,36 @@
+
+# 1. Hello World
 # print("Hello World!")
+
+# 2. We lobe kriii
 # print("강한친구 대한육군\n강한친구 대한육군")
 
+# 3. 고양이
+print("\    /\ ")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+# 4. 개
+
+
+# 5. A+B
 # A, B = map(int, input().split())
 # print(A+B)
 
+# 6. A-B
 # A, B = map(int, input().split())
 # print(A-B)
 
+# 7. AxB
 # A, B = map(int, input().split())
 # print(A*B)
 
+# 8. A/B
 # A, B = map(int, input().split())
 # print(A/B)
 
+# 9. 사칙연산
 # A, B = map(int, input().split())
 # if A >= 1 and A <= 10000 and B >=1 and B <= 10000:
 #     print(A+B)
@@ -21,6 +39,7 @@
 #     print(A//B)
 #     print(A%B)
 
+# 10. 나머지
 # A, B, C = map(int, input().split())
 # if A >= 2 and A <= 10000 and B >= 2 and B <= 10000 and C >= 2 and C <= 10000:
 #     print((A+B)%C)
@@ -28,9 +47,10 @@
 #     print((A*B)%C)
 #     print(((A%C)*(B%C))%C)
 
-A = int(input())
-B = input()
-print(A*int(B[2]))
-print(A*int(B[1]))
-print(A*int(B[0]))
-print(A*int(B))
+# 11. 곱셈
+# A = int(input())
+# B = input()
+# print(A*int(B[2]))
+# print(A*int(B[1]))
+# print(A*int(B[0]))
+# print(A*int(B))
