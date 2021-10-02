8c562cc09e781198280f525f7e670781d91244bc
datory <sehwan622@gmail.com>
2021-10-02 22:41:54 +0900

    if comparison

--- 1_입출력과 사칙연산.py
+++ 1_입출력과 사칙연산.py
@@ -1,15 +1,14 @@
-
 # 1. Hello World
-# print("Hello World!")
+print("Hello World!")
 
 # 2. We lobe kriii
-# print("강한친구 대한육군\n강한친구 대한육군")
+print("강한친구 대한육군\n강한친구 대한육군")
 
 # 3. 고양이
-# print("\    /\ ")
-# print(" )  ( ')")
-# print("(  /  )")
-# print(" \(__)|")
+print("\    /\ ")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
 
 # 4. 개
 print('|\_/|')
@@ -18,44 +17,43 @@ print('( 0 )"""\ ')
 print('|"^"`    |')
 print('||_/=\\\__|')
 
-
 # 5. A+B
-# A, B = map(int, input().split())
-# print(A+B)
+A, B = map(int, input().split())
+print(A+B)
 
 # 6. A-B
-# A, B = map(int, input().split())
-# print(A-B)
+A, B = map(int, input().split())
+print(A-B)
 
 # 7. AxB
-# A, B = map(int, input().split())
-# print(A*B)
+A, B = map(int, input().split())
+print(A*B)
 
 # 8. A/B
-# A, B = map(int, input().split())
-# print(A/B)
+A, B = map(int, input().split())
+print(A/B)
 
 # 9. 사칙연산
-# A, B = map(int, input().split())
-# if A >= 1 and A <= 10000 and B >=1 and B <= 10000:
-#     print(A+B)
-#     print(A-B)
-#     print(A*B)
-#     print(A//B)
-#     print(A%B)
+A, B = map(int, input().split())
+if A >= 1 and A <= 10000 and B >=1 and B <= 10000:
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(A//B)
+    print(A%B)
 
 # 10. 나머지
-# A, B, C = map(int, input().split())
-# if A >= 2 and A <= 10000 and B >= 2 and B <= 10000 and C >= 2 and C <= 10000:
-#     print((A+B)%C)
-#     print(((A%C)+(B%C))%C)
-#     print((A*B)%C)
-#     print(((A%C)*(B%C))%C)
+A, B, C = map(int, input().split())
+if A >= 2 and A <= 10000 and B >= 2 and B <= 10000 and C >= 2 and C <= 10000:
+    print((A+B)%C)
+    print(((A%C)+(B%C))%C)
+    print((A*B)%C)
+    print(((A%C)*(B%C))%C)
 
 # 11. 곱셈
-# A = int(input())
-# B = input()
-# print(A*int(B[2]))
-# print(A*int(B[1]))
-# print(A*int(B[0]))
-# print(A*int(B))
+A = int(input())
+B = input()
+print(A*int(B[2]))
+print(A*int(B[1]))
+print(A*int(B[0]))
+print(A*int(B))
---
+++ 2_if문.py
@@ -0,0 +1,22 @@
+# 1. 두 수 비교하기
+A, B = map(int, input().split())
+if A > B:
+    print(">")
+elif A < B:
+    print("<")
+elif A == B:
+    print("==")
+
+
+# 2. 시험 성적
+
+
+
+# 3. 윤년
+
+
+
+# 4. 사분면 고르기
+
+
+# 5. 알람 시계
